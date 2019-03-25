cf8deb160085b625ab90366a8e4b17f75ff78ee2
Sami Jouppila <sami.jouppila@gmail.com>
2019-03-25 11:43:24 +0200

    dll kansio lisätty

---
+++ BankSimulMySql/banksimulmysql.cpp
@@ -0,0 +1,6 @@
+#include "banksimulmysql.h"
+
+
+//BankSimulMySql::BankSimulMySql()
+//{
+//}
---
+++ BankSimulMySql/banksimulmysql.h
@@ -0,0 +1,13 @@
+#ifndef BANKSIMULMYSQL_H
+#define BANKSIMULMYSQL_H
+
+#include "banksimulmysql_global.h"
+
+class BANKSIMULMYSQLSHARED_EXPORT BankSimulMySql
+{
+
+public:
+    //BankSimulMySql();
+};
+
+#endif // BANKSIMULMYSQL_H
